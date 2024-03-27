c2755f5cd9be510b3b0c35b99bce59c2de1a2f9d
RedFlameKen2 <202301178@iacademy.edu.ph>
2024-03-27 17:32:24 +0800

    messed around with some js

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
 
+var pageCount = 1;
+var pages = []
+initPages();
 
-
-
+function initPages(){
+    pages.add(document.getElementById("page0"));
+    let shifted = pages.shift();
+    const book = document.getElementById("book");
+    book.removeChild(shifted);
+}
 function nextPage(){
+    if(pageCount >= 3)
+	removePage();
     const book = document.getElementById("book");
     page = document.createElement("img");
-    page.setAttribute("id", "page");
+    page.setAttribute("id", "page"+pageCount);
+    page.setAttribute("class", "page");
     page.setAttribute("src", getPageImage());
     book.appendChild(page);
+    pages.push(page);
+    if(pages < 3)
+	pageCount++;
+}
+
+function removePage(){
+    let shifted = pages.shift();
+    const book = document.getElementById("book");
+    book.removeChild(shifted);
 
 }
 
