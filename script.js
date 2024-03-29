6fc3ede0af5bc7e0f68ba838e4c4caa2efd2ba7d
RedFlameKen2 <202301178@iacademy.edu.ph>
2024-03-29 13:39:24 +0800

    added frame engine and some css stuff

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
 var pageCount = 1;
 var madePages = 1;
+var moving = false;
+var translation = 0;
+var curPage = 0;
 var pages = [];
 initPages();
 
+window.requestAnimationFrame(runLoop);
+
+function update(){
+    if(moving)
+	moveCurPage();
+}
+function draw(){
+    if(moving)
+	drawCurPageTransition();
+}
+function runLoop(){
+    update();
+    draw();
+    window.requestAnimationFrame(runLoop);
+}
 function initPages(){
     let firstElement = document.getElementById("page0");
     pages.push(firstElement);
+    curPage = firstElement;
 }
 function nextPage(){
+    moving = true;
+    curPage.style.position = "absolute";
     createPage();
     if(pageCount < 3)
 	pageCount++;
@@ -16,6 +37,15 @@ function nextPage(){
     madePages++;
 }
 
+function moveCurPage(){
+    translation -= 2.083;
+    if(translation <= -100)
+	moving = false;
+}
+
+function drawCurPageTransition(){
+    curPage.style.transform = "translateX("+translation+"%)";
+}
 function createPage(){
     const book = document.getElementById("book");
     page = document.createElement("div");
