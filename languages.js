f65669be49c97b8874ab8d574165047d411c31bc
Aethereux <euclidguillermo28@gmail.com>
2024-03-30 22:49:37 +0800

    YEs

---
+++ languages.js
@@ -0,0 +1,19 @@
+
+
+
+function changeFiloLang() {
+    let tagalogLandingPageHeader = document.getElementById("paragraph");
+    let tagalogLandingPageBtn = document.getElementById("btn-f");
+
+    tagalogLandingPageBtn.innerHTML = "Simulan Magbasa!";
+    tagalogLandingPageHeader.innerHTML = "Ang istorya ay tungkol sa mga karanasan ni Hiraya gamit ang tulong ng mahiwagang salakot. Dahil sa lungkot ni Hiraya tungkol sa kanyang kakulangan ng isang pangarap na propesyon, nanghingi siya ng payo sa kanyang lolo kung saan sinabi niya na hanapin ang kanyang salakot. Nang mahanap ito ni Hiraya, natuklasan niyang isa pala itong mahiwagang salakot. Gamit ang tulong ng salakot, naranasan ni Hiraya ang iba’t ibang mga propesyon. Pagkatapos ng kanyang paglalakbay, napagtanto niya na kahit anumang pangarap na karera ang piliin niya, hinding hindi mawawala ang mahahalagang bahagi ng sarili niya.";
+}
+
+function changeEngLang () {
+    let tagalogLandingPageHeader = document.getElementById("paragraph");
+    let tagalogLandingPageBtn = document.getElementById("btn-f");
+
+    tagalogLandingPageBtn.innerHTML = "Start Reading!";
+    tagalogLandingPageHeader.innerHTML = "The story is about Hiraya's experiences using the help of a magical hat. Because of Hiraya's sadness about her lack of a dream profession, she asked for advice from her grandfather who told her to find her hat. When Hiraya found it, she discovered that it was actually a magical hat. With the help of the hat, Hiraya experienced various professions. After her journey, she realized that no matter what dream career she chooses, the important parts of herself will never disappear.";
+}
+
